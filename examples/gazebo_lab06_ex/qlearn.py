ad3ab3450e7a7ef3d9055bd85bdf365d714336fd
gavin-pringle <gavinpringle00@gmail.com>
2022-10-30 14:15:31 -0700

    Lab completed and shown to TA

---
+++ examples/gazebo_lab06_ex/qlearn.py
@@ -0,0 +1,114 @@
+import random
+import pickle
+
+
+class QLearn:
+    def __init__(self, actions, epsilon, alpha, gamma):
+        self.q = {}
+        self.epsilon = epsilon  # exploration constant
+        self.alpha = alpha      # discount constant
+        self.gamma = gamma      # discount factor
+        self.actions = actions
+
+    def loadQ(self, filename):
+        '''
+        Load the Q state-action values from a pickle file.
+        '''
+        # TODO: Implement loading Q values from pickle file.
+        # DONE
+        with open(filename) as f:
+            self.q = pickle.load(f)
+        print("Loaded file: {}".format(filename+".pickle"))
+
+    def saveQ(self, filename):
+        '''
+        Save the Q state-action values in a pickle file.
+        '''
+        # TODO: Implement saving Q values to pickle and CSV files.
+        # DONE
+        with open(filename, 'wb') as f:
+            pickle.dump(self.q, f)
+        print("Wrote to file: {}".format(filename+".pickle"))
+
+    def getQ(self, state, action):
+        '''
+        @brief returns the state, action Q value or 0.0 if the value is 
+            missing
+        '''
+        return self.q.get((state, action), 0.0)
+
+    def chooseAction(self, state, return_q=False):
+        '''
+        @brief returns a random action epsilon % of the time or the action 
+            associated with the largest Q value in (1-epsilon)% of the time
+        '''
+        # TODO: Implement exploration vs exploitation
+        #    if we need to take a random action:
+        #       * return a random action
+        #    else:
+        #       * determine which action has the highest Q value for the state 
+        #          we are in.
+        #       * address edge cases - what if 2 actions have the same max Q 
+        #          value?
+        #       * return the action with highest Q value
+        #
+        # NOTE: if return_q is set to True return (action, q) instead of
+        #       just action
+        q = [self.getQ(state, a) for a in self.actions]
+        maxQ = max(q)
+ 
+        if random.random() < self.epsilon:
+            rand_val = random.randint(0, len(self.actions) - 1)
+            action = self.actions[rand_val]
+            if return_q: 
+                return action, q
+            return action
+            
+        count = q.count(maxQ)
+        # In case there are several state-action max values
+        # we select a random one among them
+        if count > 1:
+            best = [i for i in range(len(self.actions)) if q[i] == maxQ]
+            i = random.choice(best)
+        else:
+            i = q.index(maxQ)
+
+        # return the action taken
+        action = self.actions[i]
+        if return_q: 
+            return action, q
+        return action
+
+    def learn(self, state1, action1, reward, state2):
+        '''
+        @brief updates the Q(state,value) dictionary using the bellman update
+            equation
+        '''
+        # TODO: Implement the Bellman update function:
+        #     Q(s1, a1) += alpha * [reward(s1,a1) + gamma* max(Q(s2)) - Q(s1,a1)]
+        # 
+        # NOTE: address edge cases: i.e. 
+        # 
+        # Find Q for current (state1, action1)
+        # Address edge cases what do we want to do if the [state, action]
+        #       is not in our dictionary?
+        # Find max(Q) for state2
+        # Update Q for (state1, action1) (use discount factor gamma for future 
+        #   rewards)
+
+        # DONE
+        maxqnew = max([self.getQ(state2, a) for a in self.actions])
+        self.learnQ(state1, action1, reward, reward + self.gamma*maxqnew)
+
+    def learnQ(self, state, action, reward, value):
+        '''
+        @brief implements the Bellman update function. Helper function to learn()
+
+        Q-learning:
+            Q(s, a) += alpha * (reward(s,a) + max(Q(s') - Q(s,a))
+        '''
+        oldv = self.q.get((state, action), None)
+        if oldv is None:
+            self.q[(state, action)] = reward
+        else:
+            self.q[(state, action)] = oldv + self.alpha * (value - oldv)
--- gym_gazebo/__init__.py
+++ gym_gazebo/__init__.py
@@ -8,6 +8,7 @@ logger = logging.getLogger(__name__)
 
 # cart pole
 register(
-    id='GazeboCartPole-v0',
-    entry_point='gym_gazebo.envs.gazebo_cartpole:GazeboCartPolev0Env',
+    id='Gazebo_Lab06-v0',
+    entry_point='gym_gazebo.envs.gazebo_lab06:Gazebo_Lab06_Env',
+    max_episode_steps=3000,
 )
